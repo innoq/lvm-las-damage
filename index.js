82ba651280c38ad6cbcbb2168b84dda1c762f617
Lucas Dohmen <lucas@dohmen.io>
2016-06-10 18:10:19 +0200

    Remove auth

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const compression = require('compression')
-const inProduction = (process.env.NODE_ENV === 'production')
 const damageController = require('./lib/controllers/damage')
 
 // Set up Mustache as the view engine
@@ -12,20 +11,6 @@ app.set('view engine', 'mustache')
 app.set('layout', 'layout')
 
 app.use(bodyParser.urlencoded({ extended: false }))
-
-// BasicAuth
-if (inProduction) {
-  const passport = require('passport')
-  const BasicStrategy = require('passport-http').BasicStrategy
-  passport.use(new BasicStrategy((userid, password, done) => {
-    if (userid === 'lvm' && password === 'roca-prototype') {
-      done(null, {})
-    } else {
-      done('Unauthorized')
-    }
-  }))
-  app.use(passport.authenticate('basic', { session: false }))
-}
 
 app.locals.postbox_url = process.env.POSTBOX_URL || 'http://localhost:9000'
 app.locals.roca_url = process.env.ROCA_URL
